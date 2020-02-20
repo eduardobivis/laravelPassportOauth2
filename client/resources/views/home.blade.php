8adb6bad81d023a150c56a5ec8713c77daa6ae9f
eduardomarinhocinq <e.marinho@cinq.com.br>
2020-02-20 15:36:04 -0300

    Fixing Dropdown

--- client/resources/views/home.blade.php
+++ client/resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.js"></script>
 </head>
 
 @section('content')
