0c7b15573bf2035a9af5705ea6d92fe3c246fbbf
eduardomarinhocinq <e.marinho@cinq.com.br>
2020-02-20 11:05:25 -0300

    Some small Fixes

--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Server
                 </div>
             </div>
         </div>
